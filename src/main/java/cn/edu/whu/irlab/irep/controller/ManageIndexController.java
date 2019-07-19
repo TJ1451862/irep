2351f7d68c3a8d3f74108ce2fbe068672386a769
fangrongfu <251591587@qq.com>
2019-07-19 15:58:59 +0800

    管理页面的跳转交互

---
+++ src/main/java/cn/edu/whu/irlab/irep/controller/ManageIndexController.java
@@ -0,0 +1,20 @@
+package cn.edu.whu.irlab.irep.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author fangrf
+ * @version 1.0
+ * @date 2019-07-17 14:29
+ * @desc 后台管理的页面跳转
+ **/
+@Controller
+@RequestMapping(value = "/manage")
+public class ManageIndexController {
+
+    @RequestMapping(value = "/index.html")
+    public String indexController(){
+        return "manage/index";
+    }
+}
